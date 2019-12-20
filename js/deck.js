00acff1cd484c8c736c035e8e67bb99279898af8
EnjoyYourBan <ienjoypokemon@gmail.com>
2019-12-20 18:28:19 -0500

    Add deck.js
    Deck.js is for a card deck.

---
+++ js/deck.js
@@ -0,0 +1,67 @@
+module.exports = class Deck {
+
+  constructor(shuffle=true) {
+    this.reset(shuffle);
+  }
+
+  get() {
+    return this.cards;
+  }
+  
+  static getIndex(a) {
+    return [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', "A"].indexOf(a)
+
+  }
+
+  static randomize(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+  /**
+   * cut the deck, get a random card.
+   * @param {Bool} remove should be removed
+   * @returns {Object} card returns a card object
+   */
+  getRandom(remove=true) {
+    const numb = Math.round(Math.random()*(this.cards.length-1));
+
+    const card = this.cards[numb]
+
+    if (remove) this.cards.splice(numb, 1);
+
+    return card;
+
+  }
+
+  /**
+   * shuffle the deck, randomize it
+   * @returns {Array} list of decks cards 
+   */
+  shuffle() {
+    this.cards = Deck.randomize(this.cards)
+    return this.cards;
+  }
+  /**
+   * Reset the deck to the original state.
+   * @param {Bool} Shuffle on reset 
+   * @returns {Deck} the deck object
+   * 
+   */
+  reset(shuffle) {
+        const suites = ['spades', 'diamonds', 'hearts', 'clubs'];
+    const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', "A"];
+    this.cards = [];
+
+    for (const suit of suites) {
+      for (const card of cards) {
+        this.cards.push({suit: suit, card: card});
+      }
+    }
+
+    if(shuffle) this.shuffle();
+
+  }
+}
